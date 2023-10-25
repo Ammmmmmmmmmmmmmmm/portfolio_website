3d9c56595f9233e64643be299c00a34e2ebdc48b
Antwan <antwanmeave@tutanota.com>
2023-10-25 14:21:52 -0500

    change to game.js might stop whitespace under game

--- sh_mobile/game.js
+++ sh_mobile/game.js
@@ -11,6 +11,11 @@ window.onload = function() {
 		//parent: 'iframe-container',
 		width: 320,
 		height: 480,
+	    scale: {
+		    mode: Phaser.Scale.RESIZE,
+		    parent: 'iframe-container2', 
+		    autoCenter: Phaser.Scale.CENTER_BOTH,
+		},
 		backgroundColor: 0x000000,
 		scene: [scene1, scene2],
 		pixelArt: true,
